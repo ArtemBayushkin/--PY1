73e9a1469305947bc7578ae2c00cfa67e8db78f0
ArtemBayushkin <115110907+ArtemBayushkin@users.noreply.github.com>
2022-10-26 17:29:33 +0300

    Update task2.py
    Исправил все ошибки. Добавил процентное отношение элементов к общему их количеству.

--- Лабораторная работа 4/task2.py
+++ Лабораторная работа 4/task2.py
@@ -1,13 +1,23 @@
 def get_count_char(str_):
     dict_ = {}
-    clear_ = "".join(str_.lower().split())
+    clear_ = "".join(str_.lower())
     for i in clear_:
         if i.isalpha():
-           dict_[i] = clear_.count(i)
+            dict_[i] = dict_.get(i, 0) + 1
     return dict_
+
+
+def proc(str_):
+    str_ = {key: round(value / sum(str_.values()) * 100, 2) for key, value in str_.items()}
+    return str_
+
+
 main_str = """
     Данное предложение будет разбиваться на отдельные слова. 
-    В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
-    Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
+    В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список 
+    отдельных слов. 
+    Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. 
+    Приступим!!!!
 """
 print(get_count_char(main_str))
+print(proc(get_count_char(main_str)))
