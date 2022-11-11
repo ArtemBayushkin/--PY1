eb0d7a28588d07812f1f41776f3d3d8b641faeb4
ArtemBayushkin <115110907+ArtemBayushkin@users.noreply.github.com>
2022-11-11 13:01:14 +0300

    Update task3.py

--- Лабораторная работа 5/task3.py
+++ Лабораторная работа 5/task3.py
@@ -2,12 +2,10 @@ import random
 
 
 def get_unique_list_numbers(start=-10, stop=10, count=15) -> list[int]:
-    list_num = []
-    while len(list_num) != count:
-        number = random.randint(start, stop)
-        if number not in list_num:
-            list_num.append(number)
-    return list_num
+    list_ = list(range(start, stop))
+    random.shuffle(list_)
+    list_ = list_[:count]
+    return list_
 
 
 list_unique_numbers = get_unique_list_numbers()
