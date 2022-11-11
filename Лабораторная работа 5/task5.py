7bd4e871b7a95e63e99a63a12de75dd995a248e5
ArtemBayushkin <115110907+ArtemBayushkin@users.noreply.github.com>
2022-11-11 13:03:46 +0300

    Update task5.py

--- Лабораторная работа 5/task5.py
+++ Лабораторная работа 5/task5.py
@@ -2,8 +2,13 @@ import random
 from string import ascii_lowercase, ascii_uppercase, digits
 
 
-def get_random_password(number=8) -> str:
-    str_ = ascii_uppercase + ascii_lowercase + digits
-    return "".join(random.sample(str_,number))
+def get_random_password(length=8, uppercase=True, punctuation=True):
+    str_ = ascii_lowercase
+    if uppercase:
+        str_ += ascii_uppercase
+    if punctuation:
+        str_ += digits
+    return ''.join(random.sample(str_, length))
+
 
 print(get_random_password())
