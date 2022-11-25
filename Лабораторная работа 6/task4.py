6c32fae004ca2ee984ddfb9216215d2feba36eee
ArtemBayushkin <115110907+ArtemBayushkin@users.noreply.github.com>
2022-11-25 17:09:28 +0300

    Update task4.py

--- Лабораторная работа 6/task4.py
+++ Лабораторная работа 6/task4.py
@@ -1,8 +1,8 @@
 INPUT_FILE = "input.csv"
-OUTPUT_FILE = "output.csv"
+OUTPUT_FILE = "output.json"
 
 
-def csv_to_list_dict(file_1, file_2, delimiter=",", nl='\n') -> list[dict]:
+def csv_to_list_dict(file_1, file_2, delimiter=",", nl='\n', indent=4*" ", sk=2) -> list[dict]:
     with open(file_1) as input_:
         with open(file_2, 'w') as output:
             list_ = []
@@ -10,9 +10,17 @@ def csv_to_list_dict(file_1, file_2, delimiter=",", nl='\n') -> list[dict]:
             for line in input_:
                 data = line.rstrip(nl).split(delimiter)
                 list_.append(dict(zip(headers, data)))
+            output.write("[")
             for line1 in list_:
-                    output.write(str(line1)+nl)
+                output.write(indent*sk +
+                             str(line1).replace(delimiter, delimiter + nl + indent * sk).replace("}", nl + 
+                             indent + "},").replace("{'", nl + indent + "{" + nl + indent * sk + " '"))
+            output.write(nl+"]")
     return list_
 
 
-print(csv_to_list_dict(INPUT_FILE, OUTPUT_FILE))
+csv_to_list_dict(INPUT_FILE, OUTPUT_FILE)
+
+with open(OUTPUT_FILE) as f:
+    for line in f:
+        print(line)
